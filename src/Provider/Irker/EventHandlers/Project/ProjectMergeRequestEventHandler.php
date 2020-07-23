190101ba3e5997db1ee924d0f6b43d19fb161830
Bob van de Vijver <bob@drenso.nl>
2020-07-23 11:45:53 +0200

    Add support for merge request approvals

--- src/Provider/Irker/EventHandlers/Project/ProjectMergeRequestEventHandler.php
+++ src/Provider/Irker/EventHandlers/Project/ProjectMergeRequestEventHandler.php
@@ -18,7 +18,6 @@ class ProjectMergeRequestEventHandler extends AbstractProjectEventHandler implem
 
   public function onEvent(ProjectMergeRequestEvent $event)
   {
-
     $this->wrapHandler($event, function () use ($event) {
       switch ($event->getAction()) {
         case 'open':
@@ -38,6 +37,12 @@ class ProjectMergeRequestEventHandler extends AbstractProjectEventHandler implem
           break;
         case 'test':
           $fill = '[%s] Merge request !%s ' . IrkerUtils::colorize('test hook', IrkerUtils::COLOR_BROWN) . ' by %s: %s [ %s ]';
+          break;
+        case 'approved':
+          $fill = '[%s] Merge request !%s ' . IrkerUtils::colorize('approved', IrkerUtils::COLOR_GREEN) . ' by %s: %s [ %s ]';
+          break;
+        case 'unapproved':
+          $fill = '[%s] Merge request !%s ' . IrkerUtils::colorize('approval revoked', IrkerUtils::COLOR_BROWN) . ' by %s: %s [ %s ]';
           break;
         default:
           $fill = '[%s] Unknown action on merge request !%s by %s: %s [ %s ]';
