002f58ec81be826ce3a41351dff69aeb221f1c66
Bob van de Vijver <bob@drenso.nl>
2020-09-07 12:38:15 +0200

    Mark abstract classes as actually abstract

--- src/Provider/Irker/EventHandlers/Project/AbstractProjectEventHandler.php
+++ src/Provider/Irker/EventHandlers/Project/AbstractProjectEventHandler.php
@@ -4,11 +4,10 @@ namespace App\Provider\Irker\EventHandlers\Project;
 
 use App\Provider\Irker\EventHandlers\AbstractEventHandler;
 
-class AbstractProjectEventHandler extends AbstractEventHandler
+abstract class AbstractProjectEventHandler extends AbstractEventHandler
 {
   protected function message(string $message, ?string $channel = NULL)
   {
     parent::message($message, $channel ?? 'project');
   }
-
 }
--- src/Provider/Irker/EventHandlers/Usage/AbstractUsageEventHandler.php
+++ src/Provider/Irker/EventHandlers/Usage/AbstractUsageEventHandler.php
@@ -4,7 +4,7 @@ namespace App\Provider\Irker\EventHandlers\Usage;
 
 use App\Provider\Irker\EventHandlers\AbstractEventHandler;
 
-class AbstractUsageEventHandler extends AbstractEventHandler
+abstract class AbstractUsageEventHandler extends AbstractEventHandler
 {
   protected function message(string $message, ?string $channel = NULL)
   {
