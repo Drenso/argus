a75ba3c6a243ae39dec997302c06fb4438b9c48d
Bob van de Vijver <bob@drenso.nl>
2021-02-15 09:27:44 +0100

    Remove hashtag before commit hash in pipeline events

--- src/Provider/Irker/EventHandlers/Project/ProjectPipelineEventHandler.php
+++ src/Provider/Irker/EventHandlers/Project/ProjectPipelineEventHandler.php
@@ -24,33 +24,33 @@ class ProjectPipelineEventHandler extends AbstractProjectEventHandler implements
 
       switch ($event->getAction()) {
         case 'created':
-          $fill = '[%s] Pipeline #%s for #%s with %s job%s, submitted by %s, has been ' . IrkerUtils::colorize('created', IrkerUtils::COLOR_GREY) . ' [ %s ]';
+          $fill = '[%s] Pipeline #%s for %s with %s job%s, submitted by %s, has been ' . IrkerUtils::colorize('created', IrkerUtils::COLOR_GREY) . ' [ %s ]';
           break;
         case 'pending':
-          $fill = '[%s] Pipeline #%s for #%s with %s job%s, submitted by %s, is now ' . IrkerUtils::colorize('pending', IrkerUtils::COLOR_GREY) . ' [ %s ]';
+          $fill = '[%s] Pipeline #%s for %s with %s job%s, submitted by %s, is now ' . IrkerUtils::colorize('pending', IrkerUtils::COLOR_GREY) . ' [ %s ]';
           break;
         case 'waiting_for_resource':
-          $fill = '[%s] Pipeline #%s for #%s with %s job%s, submitted by %s, is now ' . IrkerUtils::colorize('waiting for resources', IrkerUtils::COLOR_GREY) . ' [ %s ]';
+          $fill = '[%s] Pipeline #%s for %s with %s job%s, submitted by %s, is now ' . IrkerUtils::colorize('waiting for resources', IrkerUtils::COLOR_GREY) . ' [ %s ]';
           break;
         case 'running':
-          $fill = '[%s] Pipeline #%s for #%s with %s job%s, submitted by %s, is now ' . IrkerUtils::colorize('running', IrkerUtils::COLOR_PURPLE) . ' [ %s ]';
+          $fill = '[%s] Pipeline #%s for %s with %s job%s, submitted by %s, is now ' . IrkerUtils::colorize('running', IrkerUtils::COLOR_PURPLE) . ' [ %s ]';
           break;
         case 'canceled':
         case 'cancelled':
-          $fill = '[%s] Pipeline #%s for #%s with %s job%s, submitted by %s, has been ' . IrkerUtils::colorize('cancelled', IrkerUtils::COLOR_LIGHT_RED) . ' [ %s ]';
+          $fill = '[%s] Pipeline #%s for %s with %s job%s, submitted by %s, has been ' . IrkerUtils::colorize('cancelled', IrkerUtils::COLOR_LIGHT_RED) . ' [ %s ]';
           break;
         case 'failed':
-          $fill = '[%s] Pipeline #%s for #%s with %s job%s, submitted by %s, has ' . IrkerUtils::colorize('failed', IrkerUtils::COLOR_DARK_RED) . ' [ %s ]';
+          $fill = '[%s] Pipeline #%s for %s with %s job%s, submitted by %s, has ' . IrkerUtils::colorize('failed', IrkerUtils::COLOR_DARK_RED) . ' [ %s ]';
           break;
         case 'success':
-          $fill = '[%s] Pipeline #%s for #%s with %s job%s, submitted by %s, has ' . IrkerUtils::colorize('succeeded', IrkerUtils::COLOR_GREEN) . ' [ %s ]';
+          $fill = '[%s] Pipeline #%s for %s with %s job%s, submitted by %s, has ' . IrkerUtils::colorize('succeeded', IrkerUtils::COLOR_GREEN) . ' [ %s ]';
           break;
         case 'skipped':
-          $fill     = '[%s] Pipeline #%s for #%s, submitted by %s, has been ' . IrkerUtils::colorize('skipped', IrkerUtils::COLOR_GREY) . ' [ %s ]';
+          $fill     = '[%s] Pipeline #%s for %s, submitted by %s, has been ' . IrkerUtils::colorize('skipped', IrkerUtils::COLOR_GREY) . ' [ %s ]';
           $skipJobs = true;
           break;
         default:
-          $fill     = '[%s] Pipeline #%s for #%s, submitted by %s, has an unknown action... [ %s ]';
+          $fill     = '[%s] Pipeline #%s for %s, submitted by %s, has an unknown action... [ %s ]';
           $skipJobs = true;
       }
 
