4965edeb1aca33f4246b6777c00cc2c67ed3ba81
Bob van de Vijver <bob@drenso.nl>
2021-03-03 10:57:32 +0100

    Update PHP dependencies

--- src/Serializer/Entity/ProjectSerializer.php
+++ src/Serializer/Entity/ProjectSerializer.php
@@ -5,6 +5,7 @@ namespace App\Serializer\Entity;
 use App\Entity\Project;
 use Drenso\Shared\Serializer\AbstractObjectSerializer;
 use JMS\Serializer\EventDispatcher\EventSubscriberInterface;
+use JMS\Serializer\EventDispatcher\ObjectEvent;
 use JMS\Serializer\Visitor\SerializationVisitorInterface;
 
 class ProjectSerializer extends AbstractObjectSerializer implements EventSubscriberInterface
@@ -27,11 +28,9 @@ class ProjectSerializer extends AbstractObjectSerializer implements EventSubscri
   }
 
   /**
-   * @param SerializationVisitorInterface $visitor
-   * @param array                         $groups
-   * @param Project                       $object
+   * @inheritdoc
    */
-  protected function doSerialize(SerializationVisitorInterface $visitor, array $groups, $object): void
+  protected function doSerialize(SerializationVisitorInterface $visitor, array $groups, $object, ObjectEvent $event): void
   {
     if (in_array(self::GITLAB_URL, $groups)) {
       $this->addStringProperty($visitor, self::GITLAB_URL, $this->gitlabUrl . '/' . $object->getName());
