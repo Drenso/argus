4bb95013d0dbfd622d1dc6ac0683f5238a50c737
Bob van de Vijver <bob@drenso.nl>
2021-06-07 10:12:42 +0200

    Prevent render errors when stats are not loaded yet

--- assets/vue/pages/dashboard/components/EventStats.vue
+++ assets/vue/pages/dashboard/components/EventStats.vue
@@ -1,15 +1,17 @@
 <template>
   <b-tab :title="title">
-    <b-progress :max="stats.all" show-value height="2rem" v-show="stats.all > 0">
-      <b-progress-bar :value="stats.fully_handled" variant="success"/>
-      <b-progress-bar :value="stats.partially_handled" variant="warning"/>
-      <b-progress-bar :value="stats.unhandled" variant="danger"/>
-    </b-progress>
-    <div v-show="stats.all === 0" style="height: 2rem;">
-      <div class="d-flex align-items-center h-100">
-        <span>{{ 'event.info.none-recorded'|trans }}</span>
+    <template v-if="stats">
+      <b-progress :max="stats.all" show-value height="2rem" v-show="stats.all > 0">
+        <b-progress-bar :value="stats.fully_handled" variant="success"/>
+        <b-progress-bar :value="stats.partially_handled" variant="warning"/>
+        <b-progress-bar :value="stats.unhandled" variant="danger"/>
+      </b-progress>
+      <div v-show="stats.all === 0" style="height: 2rem;">
+        <div class="d-flex align-items-center h-100">
+          <span>{{ 'event.info.none-recorded'|trans }}</span>
+        </div>
       </div>
-    </div>
+    </template>
   </b-tab>
 </template>
 
