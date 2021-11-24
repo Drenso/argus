9712b93ff14f84e2db7919ba1f9f76b2740daaa7
Bob van de Vijver <bob@drenso.nl>
2021-11-24 22:39:14 +0100

    Start messenger worker in argus container

--- docker/argus/docker-entry.sh
+++ docker/argus/docker-entry.sh
@@ -14,6 +14,15 @@ bin/console doctrine:migrations:migrate --no-interaction --query-time --allow-no
 # Remove created cache from migration
 rm -rf var/*
 
+# Start messenger daemon
+start_messenger() {
+  while true; do
+    su -s /bin/bash -c "php ${BASE_DIR}/bin/console messenger:consume async -q --no-interaction --time-limit=3600" www-data
+  done
+}
+
+start_messenger &
+
 # Original entrypoint
 docker-php-entrypoint
 php-fpm
